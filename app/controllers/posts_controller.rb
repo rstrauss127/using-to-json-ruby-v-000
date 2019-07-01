d07ebb32164ab89ba06e0bf675ef646ea6cd41c4
Rebecca Strauss <rstrauss127@gmail.com>
2019-07-01 15:42:49 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,8 +26,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: post.to_json(only: [:title, :description, :id]
-      ,include: [author: {only: [:name]}])
+    render json: post.to_json(only: [:title, :description, :id] ,include: [author: {only: [:name]}])
   end
 
   private
